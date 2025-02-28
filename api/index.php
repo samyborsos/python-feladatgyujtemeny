a1c9d6a304c964afa2fe8ffc78d970d1f82109d7
samyborsos <sygaming15@gmail.com>
2025-02-28 19:11:11 +0100

    cursor updated again

---
+++ api/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Load composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load Laravel
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
